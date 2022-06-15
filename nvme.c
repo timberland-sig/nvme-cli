c42873f91ddbfbfe1616422239a254e4aea69871
Stuart Hayes <stuart_hayes@dell.com>
2022-06-15 11:42:50 -0500

    Added NBFT table support
    Added support for parsing and printing the contents
    of the NBFT table (per NVMe-oF boot specification v1.0).
    
    Signed-off-by: Stuart Hayes <stuart_hayes@dell.com>
    Signed-off-by: John Meneghini <jmeneghi@redhat.com>
    Signed-off-by: Martin Wilck <mwilck@suse.com>

--- nvme.c
+++ nvme.c
@@ -65,6 +65,7 @@
 
 #include "util/argconfig.h"
 #include "fabrics.h"
+#include "nbft.h"
 
 #define CREATE_CMD
 #include "nvme-builtin.h"
@@ -8380,6 +8381,18 @@ static int dim_cmd(int argc, char **argv, struct command *command, struct plugin
 {
 	const char *desc = "Send Discovery Information Management command to a Discovery Controller (DC)";
 	return nvmf_dim(desc, argc, argv);
+}
+
+int connect_nbft_cmd(int argc, char **argv, struct command *command, struct plugin *plugin)
+{
+	const char *desc = "Connect subsystems listed in ACPI NBFT tables";
+	return connect_nbft(desc, argc, argv);
+}
+
+int show_nbft_cmd(int argc, char **argv, struct command *command, struct plugin *plugin)
+{
+	const char *desc = "Show ACPI NBFT table conects";
+	return show_nbft(desc, argc, argv);
 }
 
 void register_extension(struct plugin *plugin)
