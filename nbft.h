c88b080e504d38b6dd3eb27192ee7328f3c97bc4
Stuart Hayes <stuart_hayes@dell.com>
2023-03-23 16:18:22 -0500

    Move "show-nbft" command to "show" command in (new) nbft plugin
    Signed-off-by: Stuart Hayes <stuart_hayes@dell.com>

--- nbft.h
+++ nbft.h
@@ -19,7 +19,17 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#include <ccan/list/list.h>
+
 #define NBFT_SYSFS_PATH		"/sys/firmware/acpi/tables"
+
+struct nbft_file_entry {
+	struct list_node node;
+	struct nbft_info *nbft;
+};
+
+int read_nbft_files(struct list_head *nbft_list, char *path);
+void free_nbfts(struct list_head *nbft_list);
 
 extern int discover_from_nbft(nvme_root_t r, char *hostnqn_arg, char *hostid_arg,
 			      char *hostnqn_sys, char *hostid_sys,
@@ -27,4 +37,4 @@ extern int discover_from_nbft(nvme_root_t r, char *hostnqn_arg, char *hostid_arg
 			      const struct nvme_fabrics_config *cfg, char *nbft_path,
 			      enum nvme_print_flags flags, bool verbose);
 
-extern int show_nbft(const char *desc, int argc, char **argv);
+//extern int show_nbft(const char *desc, int argc, char **argv);
